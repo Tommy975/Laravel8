49255c5c66aacfb13baf098bfc37ff335836cb92
BigU <etu4105@umoncton.ca>
2021-03-29 20:24:49 -0300

    Modificationleft panel menu admin section

--- adminlteapp/resources/views/layouts/admin.blade.php
+++ adminlteapp/resources/views/layouts/admin.blade.php
@@ -177,8 +177,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         data-accordion="false">
                         <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-                        <li class="nav-item has-treeview menu-open">
-                            <a href="starter" class="nav-link active">
+  {{--                       <li class="nav-item has-treeview menu-open">
+                            <a href="#" class="nav-link active">
                                 <i class="nav-icon fas fa-tachometer-alt"></i>
                                 <p>
                                     Starter Pages
@@ -199,16 +199,19 @@ scratch. This page gets rid of all links and provides the needed markup only.
                                     </a>
                                 </li>
                             </ul>
-                        </li>
+                        </li> --}}
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="nav-icon fas fa-th"></i>
+                            <a href="" class="nav-link">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
                                 <p>
-                                    Calendar
-                                    <span class="right badge badge-danger">New</span>
+                                    Dashboard
                                 </p>
                             </a>
                         </li>
+                    </ul>
+                    <div><h3>Menu</h3></div>
+
+                    <ul>
                         <li class="nav-item">
                             <a href="#" class="nav-link">
                                 <i class="nav-icon fas fa-th"></i>
@@ -237,6 +240,35 @@ scratch. This page gets rid of all links and provides the needed markup only.
                             </a>
                         </li>
                     </ul>
+                    <li class="nav-item has-treeview menu-open">
+                        <a href="#" class="nav-link active">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <p>
+                                Authentication
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link active">
+                                    <i class="far fa-user nav-icon"></i>
+                                    <p>Users</p>r
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Roles</p>
+                                </a>
+                            </li>
+                            <li>
+                            <a href="#" class="nav-link">
+                                <i class="far fa-cirecle nav-icon"></i>
+                                <p>Permissions</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li> 
                 </nav>
                 <!-- /.sidebar-menu -->
             </div>
