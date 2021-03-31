79238eb95cadfc59178ec3a09fedba07e45a5ce9
BigU <etu4105@umoncton.ca>
2021-03-31 20:15:35 -0300

    Added Route for calendar page

--- adminlteapp/routes/web.php
+++ adminlteapp/routes/web.php
@@ -12,4 +12,6 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/starter', [App\Http\Controllers\HomeController::class, 'starter_page'])->name('starter');
 
-Route::get('/services', [App\Http\Controllers\HomeController::class, 'services_page'])->name('master');
+Route::get('/services', [App\Http\Controllers\HomeController::class, 'services_page'])->name('services');
+
+Route::get('/calendar', [App\Http\Controllers\HomeController::class, 'calendar_page'])->name('calendar');
